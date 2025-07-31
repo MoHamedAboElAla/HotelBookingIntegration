5bfee1f9dcd7f221b10fbdc99c32633d72ae2a50
Mohamed Agwa <magwa912@gmail.com>
2025-07-31 03:33:30 +0300

    resolve conflict in app-config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,39 +1,30 @@
-import { accountInterceptor } from './services/account-interceptor'; // تأكد من المسار
+import { accountInterceptor } from './services/account-interceptor';
 
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling} from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
-
-
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http'; // ✅ أضف هذا
-
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideHttpClient(),
+    provideClientHydration(withEventReplay()),
     provideToastr(),
     importProvidersFrom(FormsModule),
-    provideRouter(routes, withInMemoryScrolling({
-      anchorScrolling: 'enabled',
-      scrollPositionRestoration: 'enabled'
-    })), provideClientHydration(withEventReplay())
-
-    })),
-    provideClientHydration(withEventReplay()),
-    
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled'
+      })
+    ),
     provideHttpClient(
-      withInterceptors([accountInterceptor])
+      withInterceptors([accountInterceptor]),
+      withFetch()
     )
-
   ]
 };
-
-
