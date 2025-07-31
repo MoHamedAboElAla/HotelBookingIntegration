1b9ccfe7c24aae6fc2c33aba5fb9d05d81e059f1
MohamedAboElAla <mohamed20300005@gmail.com>
2025-07-31 14:27:17 +0300

    Modify app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,10 @@ import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListen
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
-import { accountInterceptor } from './services/account-interceptor';
 
+import { accountInterceptor } from './services/account-interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -22,24 +21,14 @@ export const appConfig: ApplicationConfig = {
       withFetch()
     ),
 
-
     provideRouter(
       routes,
       withInMemoryScrolling({
         anchorScrolling: 'enabled',
-
         scrollPositionRestoration: 'enabled'
       })
     ),
-    provideClientHydration(withEventReplay())
-  ]
-};
-
-        scrollPositionRestoration: 'enabled',
-      })
-    ),
 
     provideClientHydration(withEventReplay())
   ]
 };
-
